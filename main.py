2ef2562623ab4c2babc8c9a4f274cf7bdc659527
Eisuke15 <hideyu41691@gmail.com>
2021-06-30 20:34:52 +0900

    実行時間を制限できるようにする。

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os
-from subprocess import run
+from subprocess import run, TimeoutExpired
 
 from config import BINARIES_DIR, EXECUTION_TIME
 
@@ -7,27 +7,29 @@ from config import BINARIES_DIR, EXECUTION_TIME
 def execute_binary(binary_path):
     """バイナリファイルを実行する。
 
-    EXECUTION_TIMEを
+    EXECUTION_TIMEが経過した場合、実行を終了し、TimeoutExpiredをキャッチする。
 
     Args:
         path (string): 実行するバイナリのフルパス
 
     Returns:
         CompletedProcess: 実行結果
+        TimeoutExpired: タイムアウトエラー
 
-    Todo:
-        非同期にコマンド実行することで、実行時間も含めて時間を指定できるようにしたい。
     """
 
     # 実行権限を与える
     os.chmod(binary_path, 0o755)
 
-    # checkをTrueにするとrunが例外SubprocessErrorを投げるようになる。
-    # capture_outputをTrueにすると返り値のクラスに標準出力、標準エラー出力が格納される。
-    # shell=Trueにすると文字列をそのままシェルに渡す。Falseの場合PATHに登録された実行ファイルでないといけない。
-    # text=Trueにすると出力結果を文字列にエンコードする。
-    result = run(binary_path, shell=True, capture_output=True, check=False, text=True, timeout=EXECUTION_TIME)
-    return result
+    try:
+        # checkをTrueにすると、コマンドが異常終了した際、runが例外SubprocessErrorを投げるようになる。
+        # capture_outputをTrueにすると返り値のクラスに標準出力、標準エラー出力が格納される。
+        # shell=Trueにすると文字列をそのままシェルに渡す。Falseの場合PATHに登録された実行ファイルでないといけない。
+        # text=Trueにすると出力結果を文字列にエンコードする。
+        result = run(binary_path, shell=True, capture_output=True, check=False, text=True, timeout=EXECUTION_TIME)
+        return result
+    except TimeoutExpired as e:
+        return e
 
 
 if __name__ == "__main__":
