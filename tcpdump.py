2682121d44dd2abedb1e3ad0a3025c8bd21605bd
Eisuke15 <hideyu41691@gmail.com>
2021-07-16 21:22:35 +0900

    TcpdumpWithSpareTimeの作成

--- tcpdump.py
+++ tcpdump.py
@@ -1,4 +1,5 @@
 from subprocess import Popen
+from time import sleep
 
 
 class Tcpdump:
@@ -14,3 +15,21 @@ class Tcpdump:
 
     def __exit__(self, exc_type, exc_value, traceback):
         self.proc.terminate()
+
+
+class TcpdumpWithSpareTime(Tcpdump):
+    """パケットキャプチャ前後に待ち時間を設定できるクラス"""
+
+    def __init__(self, pcap_filepath, pre_execution_time, post_execution_time):
+        super().__init__(pcap_filepath)
+        self.pre_execution_time = pre_execution_time
+        self.post_execution_time = post_execution_time
+
+    def __enter__(self):
+        res = super().__enter__()
+        sleep(self.pre_execution_time)
+        return res
+
+    def __exit__(self, *args, **kwargs):
+        sleep(self.post_execution_time)
+        return super().__exit__(*args, **kwargs)
