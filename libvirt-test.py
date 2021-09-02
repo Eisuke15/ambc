4011f42da6413bbd8d594607c0c5d9eeded7c1d8
Eisuke15 <hideyu41691@gmail.com>
2021-09-02 21:47:54 +0900

    vmのIPアドレスを仮想DHCPサーバから取得するコードを追加

--- libvirt-test.py
+++ libvirt-test.py
@@ -2,10 +2,13 @@ import libvirt
 import sys
 
 try:
-    conn = libvirt.openReadOnly(None)
-except libvirt.libvirtError:
+    conn = libvirt.open("qemu:///system")
+except Exception as e:
     print("Failed to open connection to the hypervisor")
-    sys.exit(1)
+    exit(1)
 
+print(conn.listInterfaces())
 dom = conn.lookupByName("ubuntu20.04")
+ifaces = dom.interfaceAddresses(libvirt.VIR_DOMAIN_INTERFACE_ADDRESSES_SRC_LEASE, 0)
+print(ifaces)
 print(dom.ID(), dom.OSType(), dom.info(), dom)
