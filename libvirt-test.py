a29ea48f16403560fd22d76226744ca3d695b35d
Eisuke15 <hideyu41691@gmail.com>
2021-09-02 21:49:17 +0900

    備忘録コメントを追加

--- libvirt-test.py
+++ libvirt-test.py
@@ -10,5 +10,8 @@ except Exception as e:
 print(conn.listInterfaces())
 dom = conn.lookupByName("ubuntu20.04")
 ifaces = dom.interfaceAddresses(libvirt.VIR_DOMAIN_INTERFACE_ADDRESSES_SRC_LEASE, 0)
+# １つ目の謎の引数についてはここに詳細あり
+# https://libvirt.org/html/libvirt-libvirt-domain.html#virDomainInterfaceAddressesSource
+
 print(ifaces)
 print(dom.ID(), dom.OSType(), dom.info(), dom)
