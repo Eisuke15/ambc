98d5b60240d8493cea9c8996892e12a3f530e0b9
Eisuke15 <hideyu41691@gmail.com>
2021-09-04 01:18:39 +0900

    tcpdumpクラスを一つにまとめる

--- tcpdump.py
+++ tcpdump.py
@@ -5,31 +5,18 @@ from time import sleep
 class Tcpdump:
     """with文を用いて確実にtcpdumpを開始・終了するためのクラス"""
 
-    def __init__(self, pcap_filepath):
+    def __init__(self, pcap_filepath, interface, pre_execution_time=0, post_execution_time=0):
         self.pcap_filepath = pcap_filepath
         self.proc = None
-
-    def __enter__(self):
-        self.proc = Popen(["tcpdump", "-w", self.pcap_filepath], text=True)
-        return self
-
-    def __exit__(self, exc_type, exc_value, traceback):
-        self.proc.terminate()
-
-
-class TcpdumpWithSpareTime(Tcpdump):
-    """パケットキャプチャ前後に待ち時間を設定できるクラス"""
-
-    def __init__(self, pcap_filepath, pre_execution_time, post_execution_time):
-        super().__init__(pcap_filepath)
+        self.interface = interface
         self.pre_execution_time = pre_execution_time
         self.post_execution_time = post_execution_time
 
     def __enter__(self):
-        res = super().__enter__()
+        self.proc = Popen(["tcpdump", "-w", self.pcap_filepath, "-i", self.interface], text=True)
         sleep(self.pre_execution_time)
-        return res
+        return self
 
-    def __exit__(self, *args, **kwargs):
+    def __exit__(self, exc_type, exc_value, traceback):
         sleep(self.post_execution_time)
-        return super().__exit__(*args, **kwargs)
+        self.proc.terminate()
