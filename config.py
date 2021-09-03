60cb699b5ea4808fe04b831818ba6c1083387e97
Eisuke15 <hideyu41691@gmail.com>
2021-09-04 02:13:04 +0900

    VMを用いたテストmain関数

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
-BINARIES_DIR = './binaries'
+BINARIES_DIR = '/home/denjo/testfiles/'
 PRE_EXECUTION_TIME = 3
 POST_EXECUTION_TIME = 3
 EXECUTION_TIME_LIMIT = 10
-PCAP_DIR = './pcap'
+PCAP_DIR = '/home/denjo/pcap/'
 
 VM_USER_NAME = 'vmuser'
 KEYFILE_PATH = '/home/denjo/.ssh/id_rsa'
