a22796f285fcea97630ce05820f1cbfd4b0d1590
Eisuke15 <hideyu41691@gmail.com>
2021-09-23 21:33:26 +0900

    不要になった関数を削除

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 BINARIES_DIR = '/home/denjo/testfiles2/'
 PCAP_DIR = '/home/denjo/pcap/'
 PRE_EXECUTION_TIME = 3
-EXECUTION_TIME_LIMIT = 10
+EXECUTION_TIME_LIMIT = 20
 
 VM_USER_NAME = 'vmuser'
 KEYFILE_PATH = '/home/denjo/.ssh/id_rsa'
--- main.py
+++ main.py
@@ -1,32 +1,9 @@
 import os
-from subprocess import TimeoutExpired, run
 
-from config import (BINARIES_DIR, EXECUTION_TIME_LIMIT, PCAP_DIR,
-                    PRE_EXECUTION_TIME)
+from config import BINARIES_DIR, PCAP_DIR, PRE_EXECUTION_TIME
 from ssh import send_and_execute_file
 from tcpdump import Tcpdump
 from vm import VM
-
-
-def execute_file(filepath):
-    """ファイルを実行する。
-
-    EXECUTION_TIMEが経過した場合、実行を終了し、TimeoutExpiredをキャッチする。
-    """
-
-    # 実行権限を与える
-    os.chmod(filepath, 0o755)
-
-    try:
-        # checkをTrueにすると、コマンドが異常終了した際、runが例外SubprocessErrorを投げるようになる。
-        # capture_outputをTrueにすると返り値のクラスに標準出力、標準エラー出力が格納される。
-        # shell=Trueにすると文字列をそのままシェルに渡す。Falseの場合PATHに登録された実行ファイルでないといけない。
-        # text=Trueにすると出力結果を文字列にエンコードする。
-        result = run(filepath, shell=True, capture_output=True, check=False, text=True, timeout=EXECUTION_TIME_LIMIT)
-        return result
-    except TimeoutExpired as e:
-        return e
-
 
 if __name__ == "__main__":
 
