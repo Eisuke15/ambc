60db79f54d3fe8b57ea50fd21f811e4b2d961594
Eisuke15 <hideyu41691@gmail.com>
2021-09-23 23:08:55 +0900

    pcapが上書きされる問題を解決

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 BINARIES_DIR = '/home/denjo/testfiles2/'
-PCAP_DIR = '/home/denjo/pcap/'
+PCAP_BASE_DIR = '/home/denjo/pcap/'
 PRE_EXECUTION_TIME = 3
-EXECUTION_TIME_LIMIT = 20
+EXECUTION_TIME_LIMIT = 30
 
 VM_USER_NAME = 'vmuser'
 KEYFILE_PATH = '/home/denjo/.ssh/id_rsa'
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import os
 import time
+from datetime import datetime
 
-from config import BINARIES_DIR, PCAP_DIR, PRE_EXECUTION_TIME
+from config import BINARIES_DIR, PCAP_BASE_DIR, PRE_EXECUTION_TIME
 from ssh import send_and_execute_file
 from tcpdump import Tcpdump
 from vm import VM
@@ -15,12 +16,16 @@ if __name__ == "__main__":
         if os.path.isfile(path) and os.path.getsize(path):
             files.append(path)
 
+    pcap_dir_name = datetime.now().strftime("%Y-%m-%d-%H-%M")
+    pcap_dir = os.path.join(PCAP_BASE_DIR, pcap_dir_name)
+    os.mkdir(pcap_dir)
+
     start_time = time.time()
     for i, file in enumerate(files):
         elapsed_time = int(time.time() - start_time)
         print(f"\n{elapsed_time}[sec]  {i}/{len(files)}  {os.path.basename(file)}\n")
         with VM("ubuntu20.04", "clone-ubuntu") as vm:
-            pcap_path = os.path.join(PCAP_DIR, os.path.basename(file) + ".pcap")
+            pcap_path = os.path.join(pcap_dir, os.path.basename(file) + ".pcap")
             with Tcpdump(pcap_path, vm.interface_name, PRE_EXECUTION_TIME):
                 send_and_execute_file(file, vm.ip_addr)
 
