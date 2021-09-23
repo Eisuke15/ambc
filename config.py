a56dfadc85f107a60695e0a73bc0c6f93359d985
Eisuke15 <hideyu41691@gmail.com>
2021-09-23 21:27:44 +0900

    不要な定数を削除

--- config.py
+++ config.py
@@ -1,8 +1,7 @@
 BINARIES_DIR = '/home/denjo/testfiles2/'
-PRE_EXECUTION_TIME = 3
-POST_EXECUTION_TIME = 10
-EXECUTION_TIME_LIMIT = 10
 PCAP_DIR = '/home/denjo/pcap/'
+PRE_EXECUTION_TIME = 3
+EXECUTION_TIME_LIMIT = 10
 
 VM_USER_NAME = 'vmuser'
 KEYFILE_PATH = '/home/denjo/.ssh/id_rsa'
