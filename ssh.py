94fce9cc183f6cb84c4c2107b741107c75f72a7e
Eisuke15 <hideyu41691@gmail.com>
2021-09-27 16:01:12 +0900

    ssh周りの関数を分割

--- ssh.py
+++ ssh.py
@@ -9,6 +9,27 @@ import paramiko
 from config import EXECUTION_TIME_LIMIT, KEYFILE_PATH, VM_USER_NAME
 
 
+def send_file(client, filepath):
+    """SFTPでファイルの送信"""
+
+    filename = os.path.basename(filepath)
+    try:
+        sftp_connection = client.open_sftp()
+        vm_path = f'/home/{VM_USER_NAME}/{filename}'
+        sftp_connection.put(filepath, vm_path)
+        sftp_connection.chmod(vm_path, 0o755)
+    except FileNotFoundError as e:
+        print(e, file=sys.stderr)
+        sys.exit(1)
+
+
+def connect_and_send_file(ip_addr, filepath):
+    with paramiko.SSHClient() as client:
+        client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        client.connect(ip_addr, username=VM_USER_NAME, key_filename=KEYFILE_PATH, timeout=1)
+        send_file(client, filepath)
+
+
 def send_and_execute_file(filepath, ip_addr):
     """ファイルを送信して実行し、結果を得るファイル。
 
@@ -25,14 +46,7 @@ def send_and_execute_file(filepath, ip_addr):
         client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         client.connect(ip_addr, username=VM_USER_NAME, key_filename=KEYFILE_PATH, timeout=1)
 
-        try:
-            sftp_connection = client.open_sftp()
-            vm_path = f'/home/{VM_USER_NAME}/{filename}'
-            sftp_connection.put(filepath, vm_path)
-            sftp_connection.chmod(vm_path, 0o755)
-        except FileNotFoundError as e:
-            print(e, file=sys.stderr)
-            sys.exit(1)
+        send_file(client, filepath)
 
         print(f"command: ./{filename}")
 
