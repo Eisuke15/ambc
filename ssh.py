2a89cb64be224691b0f258b56bf94e99424e3ba8
Eisuke15 <hideyu41691@gmail.com>
2021-10-16 01:28:51 +0900

    SSHクラスにexecute_fileを追加

--- ssh.py
+++ ssh.py
@@ -5,58 +5,6 @@ from time import sleep
 
 import paramiko
 
-from settings import EXECUTION_TIME_LIMIT, KEYFILE_PATH, VM_USER_NAME
-
-
-def send_and_execute_file(filepath, ip_addr):
-    """ファイルを送信して実行し、結果を得る関数。
-
-    Args:
-        filepath (str): 送信、実行するファイルのフルパス
-        ip_addr (str): 送信先のIPアドレス
-
-    Returns:
-        stdout_text: 標準出力をutf-8でデコードしたもの
-        stderr_text: 標準エラー出力をutf-8でデコードしたもの
-    """
-
-    filename = os.path.basename(filepath)
-    with paramiko.SSHClient() as client:
-        client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        client.connect(ip_addr, username=VM_USER_NAME, key_filename=KEYFILE_PATH, timeout=1)
-
-        send_file(client, filepath)
-
-        print(f"command: ./{filename}")
-
-        # 別のスレッドで、実行時間制限だけsleepする関数を実行する。
-        # これにより、もしexec_command関数で実行したコマンドが時間内に終了しても、
-        # 制限時間までスレッドがjoinするのを待つことで毎回一定時間パケットキャプチャするようにしている。
-        thread = threading.Thread(target=execution_time_limit, args=(EXECUTION_TIME_LIMIT,))
-        thread.start()
-
-        try:
-            _, stdout, stderr = client.exec_command(f"./{filename}", timeout=EXECUTION_TIME_LIMIT)
-
-            print("\n****************stdout*****************")
-            for line in stdout:
-                print(line, end="")
-
-            print("\n****************stderr*****************")
-            for line in stderr:
-                print(line, end="")
-        except socket.timeout:
-            print("\n実行時間制限\n")
-        else:
-            returncode = stdout.channel.recv_exit_status()
-            print(f"\n実行終了 return code = {returncode}\n")
-        finally:
-            thread.join()
-
-
-def execution_time_limit(time):
-    sleep(time)
-
 
 class SSH:
     """with文でsshコネクションを管理する
@@ -95,6 +43,43 @@ class SSH:
             sftp_conn.put(local_specimen_path, remote_specimen_path)
             sftp_conn.chmod(remote_specimen_path, 0o755)
 
+    def execute_file(self, remote_specimen_path, ovservation_time):
+        """検体を実行し、VM上での検体の出力を出力しながら、一定時間待機する。
+
+        Args:
+            local_specimen_path (str): 送信、実行するファイルのパス
+            remote_specimen_path (str): VM内のファイルの置き場
+        """
+
+        # 別のスレッドで、実行時間制限だけsleepする関数を実行する。
+        # これにより、もしexec_command関数で実行したコマンドが時間内に終了しても、
+        # 制限時間までスレッドがjoinするのを待つことで毎回一定時間パケットキャプチャするようにしている。
+        def execution_time_limit(time):
+            sleep(time)
+        thread = threading.Thread(target=execution_time_limit, args=(ovservation_time,))
+        thread.start()
+
+        try:
+            command = f"{remote_specimen_path}"
+            print(f"Command: {command}")
+            _, stdout, stderr = self.client.exec_command(command, timeout=ovservation_time)
+
+            print("\n****************stdout*****************")
+            for line in stdout:
+                print(line, end="")
+
+            print("\n****************stderr*****************")
+            for line in stderr:
+                print(line, end="")
+        except socket.timeout:
+            print("\n実行時間制限\n")
+        else:
+            returncode = stdout.channel.recv_exit_status()
+            print(f"\n実行終了 return code = {returncode}\n")
+        finally:
+            thread.join()
+            print("パケット観測終了")
+
     def wait_until_receive(self, local_dir_path: str, remote_dir_path: str):
         """指定されたパスを監視する．ファイルを発見したら受信して削除し，そのパスを返す．
 
@@ -128,8 +113,10 @@ class SSH:
 
 if __name__ == "__main__":
     from settings import (HONEYPOT_IP_ADDR, HONEYPOT_SPECIMEN_DIR,
-                          HONEYPOT_SSH_PORT, HONEYPOT_USER_NAME,
-                          TMP_SPECIMEN_DIR)
+                          HONEYPOT_SSH_PORT, HONEYPOT_USER_NAME, KEYFILE_PATH,
+                          TMP_SPECIMEN_DIR, VM_USER_NAME)
 
-    with SSH(HONEYPOT_IP_ADDR, HONEYPOT_USER_NAME, KEYFILE_PATH, HONEYPOT_SSH_PORT) as ssh:
-        print(ssh.wait_until_receive(TMP_SPECIMEN_DIR, HONEYPOT_SPECIMEN_DIR))
+    with SSH("192.168.122.69", VM_USER_NAME, KEYFILE_PATH) as ssh:
+        remote_specimen_path = f"/home/{VM_USER_NAME}/test.sh"
+        ssh.send_file("/home/denjo/test.sh", remote_specimen_path)
+        ssh.execute_file(remote_specimen_path, 20)
